60da34e2acc8b52665c242cabbc3f7f9a9816915
walk1ng <iliwgg@gmail.com>
2024-07-14 19:21:07 +0800

    添加接口：创建内容

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+// 使用中间件
+app.use(express.json())
+
 app.listen(port, () => {
     console.log(` 🎉 服务已启动在端口 ${port}...`)
 });
@@ -37,4 +40,23 @@ app.get('/posts/:postId',(req,res)=>{
     const { postId } = req.params
     const posts = data.filter(item => item.id == postId)
     res.send(posts[0])
+})
+
+app.post('/posts',(req,res)=>{
+
+    // 获取请求体中的内容
+    const { content } = req.body
+
+    // 获取请求头中的内容
+    const whoami = req.header('whoami')
+    console.log(whoami);
+    
+    // 设置响应头中的内容
+    res.setHeader('whoami', 'iambackend')
+
+    // 设置http code 并发送响应
+    res.status(201).send({
+        "message": `创建成功: ${content}`
+    })
+    
 })
