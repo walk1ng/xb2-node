93ac6ee7a3e46ea46248983e29ca4d6a0014d37a
walk1ng <iliwgg@gmail.com>
2024-07-16 11:37:35 +0800

    安装types@express, 并为res,req设置类型

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-const express = require('express');
+import express from 'express';
+import { Request, Response } from 'express';
 const app = express();
 const port = 4000;
 
 // 使用中间件
-app.use(express.json())
+app.use(express.json());
 
 app.listen(port, () => {
-    console.log(` 🎉 服务已启动在端口 ${port}...`)
+    console.log(` 🎉 服务已启动在端口 ${port}...`);
 });
 
-app.get('/', (req, res) => {
-    res.send('你好!')
-})
+app.get('/', (req: Request, res: Response) => {
+    res.send('你好!');
+});
 
 const data = [
     {
@@ -30,33 +31,32 @@ const data = [
         title: '标题3',
         content: '内容3'
     }
-]
+];
 
-app.get('/posts',(req,res)=>{
-    res.send(data)
-})
+app.get('/posts',(req: Request,res: Response)=>{
+    res.send(data);
+});
 
-app.get('/posts/:postId',(req,res)=>{
-    const { postId } = req.params
-    const posts = data.filter(item => item.id == postId)
-    res.send(posts[0])
-})
+app.get('/posts/:postId',(req: Request,res: Response)=>{
+    const { postId } = req.params;
+    const posts = data.filter(item => item.id == postId);
+    res.send(posts[0]);
+});
 
-app.post('/posts',(req,res)=>{
+app.post('/posts',(req: Request,res: Response)=>{
 
     // 获取请求体中的内容
-    const { content } = req.body
+    const { content } = req.body;
 
     // 获取请求头中的内容
-    const whoami = req.header('whoami')
+    const whoami = req.header('whoami');
     console.log(whoami);
     
     // 设置响应头中的内容
-    res.setHeader('whoami', 'iambackend')
+    res.setHeader('whoami', 'iambackend');
 
     // 设置http code 并发送响应
     res.status(201).send({
         "message": `创建成功: ${content}`
-    })
-    
-})
+    });
+});
