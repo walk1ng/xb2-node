3156ba21354e758a3f0a3bd5e3d3a083e3cc07a7
walk1ng <iliwgg@gmail.com>
2024-07-16 13:36:06 +0800

    使用prettier

--- src/main.ts
+++ src/main.ts
@@ -7,56 +7,54 @@ const port = 4000;
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(` 🎉 服务已启动在端口 ${port}...`);
+  console.log(` 🎉 服务已启动在端口 ${port}...`);
 });
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('你好!');
+  res.send('你好!');
 });
 
 const data = [
-    {
-        id: 1,
-        title: '标题1',
-        content: '内容1'
-    },
-    {
-
-        id: 2,
-        title: '标题2',
-        content: '内容2'
-    },
-    {
-        id: 3,
-        title: '标题3',
-        content: '内容3'
-    }
+  {
+    id: 1,
+    title: '标题1',
+    content: '内容1',
+  },
+  {
+    id: 2,
+    title: '标题2',
+    content: '内容2',
+  },
+  {
+    id: 3,
+    title: '标题3',
+    content: '内容3',
+  },
 ];
 
-app.get('/posts',(req: Request,res: Response)=>{
-    res.send(data);
+app.get('/posts', (req: Request, res: Response) => {
+  res.send(data);
 });
 
-app.get('/posts/:postId',(req: Request,res: Response)=>{
-    const { postId } = req.params;
-    const posts = data.filter(item => item.id == parseInt(postId, 10));
-    res.send(posts[0]);
+app.get('/posts/:postId', (req: Request, res: Response) => {
+  const { postId } = req.params;
+  const posts = data.filter((item) => item.id == parseInt(postId, 10));
+  res.send(posts[0]);
 });
 
-app.post('/posts',(req: Request,res: Response)=>{
+app.post('/posts', (req: Request, res: Response) => {
+  // 获取请求体中的内容
+  const { content } = req.body;
 
-    // 获取请求体中的内容
-    const { content } = req.body;
+  // 获取请求头中的内容
+  const whoami = req.header('whoami');
+  console.log(whoami);
 
-    // 获取请求头中的内容
-    const whoami = req.header('whoami');
-    console.log(whoami);
-    
-    // 设置响应头中的内容
-    res.setHeader('whoami', 'iambackend');
+  // 设置响应头中的内容
+  res.setHeader('whoami', 'iambackend');
 
-    // 设置http code 并发送响应
-    res.status(201).send({
-        "message": `创建成功: ${content}`
-    });
+  // 设置http code 并发送响应
+  res.status(201).send({
+    message: `创建成功: ${content}`,
+  });
 });
