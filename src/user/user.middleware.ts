32c7ddddde207bcdbec58d88d82e00c9147986fb
walk1ng <iliwgg@gmail.com>
2024-07-21 17:28:08 +0800

    HASH 密码 bcrypt

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from './user.service';
+import bcrypt from 'bcrypt';
 
 /**
  * 验证用户数据
@@ -9,7 +10,7 @@ export const validateUserData = async (
   res: Response,
   next: NextFunction,
 ) => {
-  console.log('👮🏻 验证用户数据');
+  console.log('👮🏻验证用户数据');
 
   // 准备数据
   const { name, password } = req.body;
@@ -21,6 +22,25 @@ export const validateUserData = async (
   // 验证用户是否存在
   const user = await userService.getUserByName(name);
   if (user) return next(new Error('USER_ALREADY_EXIST'));
+
+  // 下一步
+  next();
+};
+
+/**
+ * HASH 密码
+ */
+export const hashPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log('🔒 HASH 密码');
+
+  const { password } = req.body;
+
+  // HASH 密码
+  req.body.password = await bcrypt.hash(password, 10);
 
   // 下一步
   next();
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,13 +1,13 @@
 import * as userController from './user.controller';
 import { Router } from 'express';
-import { validateUserData } from './user.middleware';
+import { validateUserData, hashPassword } from './user.middleware';
 
 const router = Router();
 
 /**
  * 创建用户
  */
-router.post('/user', validateUserData, userController.store);
+router.post('/user', validateUserData, hashPassword, userController.store);
 
 // 导出 user router
 export default router;
