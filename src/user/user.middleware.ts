6870f67016ca54753dcd68e21232f7c23d848d22
walk1ng <iliwgg@gmail.com>
2024-07-21 16:51:25 +0800

    验证用户数据

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,2 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from './user.service';
+
+/**
+ * 验证用户数据
+ */
+export const validateUserData = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log('👮🏻 验证用户数据');
+
+  // 准备数据
+  const { name, password } = req.body;
+
+  // 验证数据
+  if (!name) return next(new Error('NAME_IS_REQUIRED'));
+  if (!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+
+  // 下一步
+  next();
+};
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,12 +1,13 @@
 import * as userController from './user.controller';
 import { Router } from 'express';
+import { validateUserData } from './user.middleware';
 
 const router = Router();
 
 /**
  * 创建用户
  */
-router.post('/user', userController.store);
+router.post('/user', validateUserData, userController.store);
 
 // 导出 user router
 export default router;
