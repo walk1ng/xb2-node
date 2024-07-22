7f054b27bcf2feffddffa9e6fbb4fdc7cce72c2e
walk1ng <iliwgg@gmail.com>
2024-07-22 20:35:49 +0800

    用户登录

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,2 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 import * as authService from './auth.service';
+
+/**
+ * 用户登录
+ */
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { name, password } = req.body;
+
+  try {
+    // 发送响应
+    res.send({ message: `欢迎回来，${name}` });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -3,4 +3,10 @@ import * as authController from './auth.controller';
 
 const router = express.Router();
 
+/**
+ * 用户登录
+ */
+router.post('/login', authController.login);
+
+// 导出路由
 export default router;
