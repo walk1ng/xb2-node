02eaae43bf9c31ff93969c2c01410d3356998d6d
walk1ng <iliwgg@gmail.com>
2024-07-24 22:06:48 +0800

    有趣的

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import app from './app';
 import { APP_PORT } from './app/app.config';
 import { connection } from './app/database/mysql';
 
-app.listen(APP_PORT, () => console.log('Server is running'));
+app.listen(APP_PORT, () => console.log('☘️ Server is running'));
 
 /**
  * 测试数据库连接
  */
 connection.connect((err) => {
   if (err) {
-    console.error('连接数据库失败: ' + err.stack);
+    console.error('😵 连接数据库失败: ' + err.stack);
     return;
   }
-  console.log('连接数据库成功，connected as id ' + connection.threadId);
+  console.log('🚛 连接数据库成功，connected as id ' + connection.threadId);
 });
