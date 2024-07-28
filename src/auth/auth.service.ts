b5a03f4ce1f38d6ff7ea4d399d5651e3a41a630a
walk1ng <iliwgg@gmail.com>
2024-07-28 10:41:21 +0800

    定义检查用户是否拥有指定资源的服务

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { PRIVATE_KEY } from '../app/app.config';
+import { connection } from '../app/database/mysql';
 
 /**
  * 签发信息
@@ -19,4 +20,33 @@ export const signToken = (options: SignTokenOptions) => {
 
   // 提供token
   return token;
+};
+
+/**
+ * 检查用户是否拥有某个资源
+ */
+interface PossessOptions {
+  userId: number;
+  resourceId: number;
+  resourceType: string;
+}
+
+export const possess = async (options: PossessOptions) => {
+  // 准备选项
+  const { userId, resourceId, resourceType } = options;
+
+  // 准备查询
+  const statement = `
+  SELECT COUNT(${resourceType}.id) AS count
+  FROM ${resourceType}
+  WHERE ${resourceType}.id = ? AND userId = ?
+  `;
+
+  // 执行查询
+  const [data] = await connection
+    .promise()
+    .query(statement, [resourceId, userId]);
+
+  // 返回检查结果
+  return data[0].count > 0;
 };
